4ac31f8fd04d863305635dc86e0fd0fba3b951bc
Arthur Rodrigues <arthrodrigues25@gmail.com>
2025-03-16 14:38:55 -0300

    tetst

--- projects/election.py
+++ projects/election.py
@@ -7,4 +7,26 @@
 ## count on votes in  states
 ## percentage on votes in states
 
+class Brazil():
+    def __init__(self): # The only object concrete
+        self.states = [] #List containing the states
+        self.population = [] #List containing the population
 
+    def add_states(self, states, population):
+        self.states.append(states)
+        self.population.append(population)
+
+    def __str__(self):
+       result = 'States and Population'
+       for i in range(len(self.states)):
+           return += f'{self.states[i]} - e'
+    
+
+
+if __name__ == '__main__':
+    for _ in range(2):
+        brazil = Brazil(
+        str(input('Whats you states? ')),
+        float(input('Enter you population: '))
+    )
+print(brazil)
