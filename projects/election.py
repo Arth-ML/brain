0eb2e90a831c3f8f48a237b65f12d0cbcb6c68b0
Arthur Rodrigues <arthrodrigues25@gmail.com>
2025-03-16 12:02:29 -0300

    prototype

---
+++ projects/election.py
@@ -0,0 +1,10 @@
+## Creat States
+## The number population
+## eligible to vote of states
+## create pre-president
+## choose two pre-president among the states
+## count on votes in country
+## count on votes in  states
+## percentage on votes in states
+
+
